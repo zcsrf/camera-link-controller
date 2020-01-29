7b3a8be1c48751b0deb3923710d1ec040a773c92
José Ferreira <zcsrferreira@gmail.com>
2020-01-29 23:33:07 +0000

    ADD: Working camera in canvas
    * Camera is now working properly on the canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,49 @@
-var imageCapture;
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas-small');
+var context = canvas.getContext('2d');
+
+function calculateSize(srcSize, dstSize) {
+  var srcRatio = srcSize.width / srcSize.height;
+  var dstRatio = dstSize.width / dstSize.height;
+  if (dstRatio > srcRatio) {
+    return {
+      width:  dstSize.height * srcRatio,
+      height: dstSize.height
+    };
+  } else {
+    return {
+      width:  dstSize.width,
+      height: dstSize.width / srcRatio
+    };
+  }
+}
+
+function renderFrame() {
+  requestAnimationFrame(renderFrame);
+  canvas.width = canvas.scrollWidth;
+  canvas.height = canvas.scrollHeight;
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    var videoSize = { width: video.videoWidth, height: video.videoHeight };
+    var canvasSize = { width: canvas.width, height: canvas.height };
+    var renderSize = calculateSize(videoSize, canvasSize);
+    var xOffset = (canvasSize.width - renderSize.width) / 2;
+    context.drawImage(video, xOffset, 0, renderSize.width, renderSize.height);
+  }
+}
+
+/*if (navigator.mediaDevices.getUserMedia) {
+  var successCallback = function(stream) {
+    video.srcObject = stream;
+  };
+  var errorCallback = function(error) {
+    console.log(error);
+  };
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+	video: {width: {exact: 640}, height: {exact: 480}}
+  }).then(successCallback, errorCallback);
+  requestAnimationFrame(renderFrame);
+}*/
 
 window.onload = function () {
 
@@ -6,10 +51,13 @@ navigator.mediaDevices.getUserMedia({video: true}).then(mediaStream => {
     document.querySelector('video').srcObject = mediaStream;
     const track = mediaStream.getVideoTracks()[0];
     imageCapture = new ImageCapture(track);
+	requestAnimationFrame(renderFrame);
+
+
   }).catch(function(error){
     console.log(error);
     const canvas = document.getElementById("canvas-small");
-          const mCtx = canvas.getContext("2d");
+    const mCtx = canvas.getContext("2d");
     const img = new Image();
     img.src = "assets/img/default.png";
     img.onload = () => {
@@ -61,7 +109,7 @@ function onTakePhotoButtonClick() {
 function downloadImage(blob){
   // uses the <a download> to download a Blob
   let a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
+  a.href.srcObject = (blob);
   a.download = 'image.png';
   document.body.appendChild(a);
   a.click();
